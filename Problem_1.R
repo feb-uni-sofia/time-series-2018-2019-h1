d24f1b928a9d74d2cab6b39518262278cf78192f
Boyko Amarov <boyko.amarov@gmail.com>
2018-11-30 10:52:20 +0200

    Upload Problem1.R stub and update README

---
+++ Problem_1.R
@@ -0,0 +1,38 @@
+## Load libraries
+## If the xts library is not installed on your computer run
+## install.packages('xts')
+## to install it.
+
+library(xts)
+
+store <- read.csv('https://s3.eu-central-1.amazonaws.com/sf-timeseries/data/rossmann1.csv', stringsAsFactors = FALSE)
+
+## a)
+
+## b)
+
+## c)
+
+## d)
+
+## e)
+
+## f)
+
+## g)
+
+## h)
+
+## i)
+## use the to.monthly function
+
+
+## j)
+
+## k)
+
+## l)
+
+## m)
+
+## n)
